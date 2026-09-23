cbd45cc9cdd02b329fc1bb9c826a7b16e0babd9b
MapsJuna <jsetati@googlemail.com>
2026-09-23 22:38:35 +0200

    modified tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -7,16 +7,16 @@ import { validUsers } from "../testdata/TestData";
 
 test.describe('Login Tests', () => {
 
-    test('Admin should be able to login', async ({ page, loginPage }) => {
+    test('Admin should be able to login', async ({ loginPage }) => {
 
-        await page.goto('https://ndosisimplifiedautomation.vercel.app');
+        await loginPage.GoToUrl('https://ndosisimplifiedautomation.vercel.app');
         await loginPage.navigateToLoginPage();
         await loginPage.userLogin(validUsers.admin.username,validUsers.admin.password);
 
        
     });
 
-    test('User should be able to login', async ({ page, loginPage }) => {
+    test('User should be able to login', async ({ loginPage }) => {
 
         await loginPage.GoToUrl('https://ndosisimplifiedautomation.vercel.app');
         await loginPage.navigateToLoginPage();
